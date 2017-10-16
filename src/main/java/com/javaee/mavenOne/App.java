9f809de11850305364a142089b64cb42c7374aed
sdyer777 <sdyer777@yahoo.com>
2017-10-16 19:47:33 -0400

    First commit. Tarjan working.

---
+++ src/main/java/com/javaee/mavenOne/App.java
@@ -0,0 +1,82 @@
+package com.javaee.mavenOne;
+
+import java.util.Arrays;
+
+/**
+ * Coding exercise for Nabil at MassMutual.
+ * 
+ * Given a number of locations and he directional paths between them, calculate:
+ * - the number of locations that can reach all others
+ * - the number of locations that can be reached by all others
+ * 
+ * This is an exercise in graph manipulation and traversal.  The code does the following:
+ * - create graph node list from the "paths" parameter, the input to the exercise
+ * - use Tarjan's algorithm to reduce cycles to Strongly Connected Components (SCCs)
+ * - iterate over the SCCs to find sources and sinks
+ * - "to all" and "from all" rely on their being exactly ONE source/sink, so if 
+ *   exactly one is found, the number of nodes in that SCC is the desired result.
+ *
+ */
+public class App 
+{
+    /**
+     * Helper function to organize test path string arrays.  Set the scenario to process on the first line in MAIN below.
+     * 
+     * @param scenario - which test patch scenario to return
+     */
+    private static String[] getPaths(int scenario) {
+    	switch (scenario) {
+    		/** exercise example 0.  Returns 1,1 */
+    		case 0: return new String[] {"010", "000", "110"};
+     		/** exercise example 1.  Returns 1,3 */
+    		case 1: return new String[] {"0010", "1000", "1100", "1000"};
+        	/** exercise example 2.  Returns 5,5 */
+        	case 2: return new String[] {"01000", "00100", "00010", "00001", "10000"};
+       		/** exercise example 3.  Returns 1,3 */
+        	case 3: return new String[] {"0110000","1000100","0000001","0010000","0110000","1000010","0001000"};        			
+       		/** too few edges (two edges for four nodes).  Returns 0,0 */
+        	case 4: return new String[] {"0000", "0000", "1000", "1000"}; 
+        	/** simple two-node, one-scc graph.  Returns 2,2 */
+        	case 5: return new String[] {"01", "10"};  
+        	/** larger graph of 12 nodes.  Returns 0, 10*/
+        	case 6: return new String[] {"000010000000", "101000000010", "000000000010", "000011000000", "100000000010", "000000001000", "010100000000", "000000000010", "000101010000", "000000010000", "011000000001", "000000001000"};  
+        	/** yet larger graph of 14 nodes.  Same 12 nodes as above, with two nodes above indexes 9 and 6, node index 13 being a "can reach all" source.  Returns 1, 10*/
+        	case 7: return new String[] {"00001000000000", "10100000001000", "00000000001000", "00001100000000", "10000000001000", "00000000100000", "01010000000000", "00000000001000", "00010101000000", "00000001000000", "01100000000100", "00000000100000", "00000010010000", "00000000000010"};  
+    		/** Four nodes and 3 edges, two sources and two sinks.  No cycles so no SCCs.  I.e. raw, pre-SCC graph is a forest.  Returns 0,0 */
+    		case 8: return new String[] {"0110", "0000", "0000", "0100"};
+    		/** Four nodes in two non-bridged cycles.  I.e. the post-SCC DAG is a forest.  Returns 0,0 */
+    		case 9: return new String[] {"0100", "1000", "0001", "0010"};
+        	/** default - simple two-node, one-scc graph, should return 2,2 (above as case 5) */
+        	default: return new String[] {"01", "10"};  
+    	}
+    }
+    
+    /**
+     * Main program call.
+     * 
+     * Creates a TeamBuilder object, processes the specified path scenario, writes the "can reach all"
+     *  and "reached by all" counts to the console.
+     *  
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        /** Set the scenario to test */
+    	int scenario = 9;
+    	
+    	/** Retrieve the scenario's path string array from the helper getPaths function. */
+        String[] paths = getPaths(scenario);
+        System.out.println("Path scenario: " + Arrays.toString(paths));
+        
+    	/** Create TeamBuilder object, the main object in the exercise. */
+        TeamBuilder tb = new TeamBuilder();
+        
+        /** Calculate the special location counts */
+        int[] counts = tb.specialLocations(paths);
+        
+        /** Write the results to the console */
+        System.out.println("\nRESULTS:\nCan Reach All = " + counts[0] + "\nReached By All = " + counts[1]);
+    }
+    
+    
+}
---
+++ src/main/java/com/javaee/mavenOne/TeamBuilder.java
@@ -0,0 +1,124 @@
+package com.javaee.mavenOne;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+import java.util.Stack;
+
+import com.javaee.mavenOne.tarjan.*;
+
+/**
+ * This class is specified in the exercise as to be created. The one function, specialLocations, executes the logic for 
+ * calculating the desired counts.
+ * 
+ * @author stevendyer
+ *
+ */
+public class TeamBuilder {
+	
+	/**
+	 * Calculate number of "Special Locations".
+	 * 
+	 * Main processing function, taking in a graph (as an array of strings of outgoing edge lists) and returning
+	 * the number of "special locations" - those that can reach or be reached by all other nodes.
+	 * 
+	 * @param paths - array of nodes, each a string containing a series of 0's or 1's, 1 indicating a link to a node.
+	 * 
+	 * @return int[] - first number is count of nodes that can reach all others, 
+	 * 	               second is count of those that can be reached by all others
+	 */
+	public int[] specialLocations(String[] paths) {
+		/** Set number of raw nodes in our graph, i.e. the number of strings in the paths array. */
+		int numNodes = paths.length;
+		
+        /** From the path string array, construct graph.  I.e. a list of nodes and edges. And count the edges */
+        List<Integer>[] nodeList = new List[numNodes];
+        int edgeCount = 0;
+        for (int i = 0; i < numNodes; i++)
+        {
+        	nodeList[i] = new ArrayList<Integer>();        
+        	for (int j = 0; j < paths[i].length(); j++) {
+        		if (paths[i].charAt(j) != '0') {
+        			nodeList[i].add(j); 
+        			edgeCount++;
+        		}
+        	}
+        }
+        /** Output the nodelist and edge count to the console so the user can follow along. */
+        System.out.println("\nNodes and their outgoing edges (nodeList) : ");
+        for (int i = 0; i < numNodes; i++)
+        {
+        	System.out.println(i + " " + nodeList[i]);
+        }
+        System.out.println("edgeCount = " + edgeCount);
+        
+        /** If too few edges, it's a forest, so return {0,0} */
+        if (edgeCount < numNodes - 1) {
+        	System.out.println("Too few edges, returning {0,0}");
+        	return new int[]{0,0};
+        }
+		
+        /** Create a tarjan object and have it calculate the Strongly Connected Components. */
+        Tarjan tarjan = new Tarjan();
+        List<List<Integer>> scComponents = tarjan.getSCComponents(nodeList);
+        /** Print the SCC list and the map of the original nodes to the SCCs **/
+        System.out.println("\nTarjan-calculated SCC's (scComponents) : \n"+ scComponents);        
+        System.out.println("\nSCC that each node is in (nodeSccMap) : \n" + Arrays.toString(tarjan.nodeSccMap));
+
+        /** 
+         * Using the original nodeList and the map of those nodes to SCCs.
+         * The result is two bitsets, one bit per SCC, indicating which SCCs have incoming/outgoing edges.
+         */
+        BitSet sccHasIncoming = new BitSet(tarjan.sccCount);
+        BitSet sccHasOutgoing = new BitSet(tarjan.sccCount);
+        /** Run through the nodes in the original graph */
+        for (int i = 0; i < nodeList.length; i++)
+        {
+        	/** evaluate each outgoing edge in the node */
+        	for (int j : nodeList[i]) {
+	    		/** If nodes are in different SCC's, update the source and sink data for the scc's involved in the edge.
+	    		 * Note: we ignore edges within the same SCC - i.e. both nodes i and j map to the same SCC.
+	    		 */
+	    		if (tarjan.nodeSccMap[i] != tarjan.nodeSccMap[j]) {
+	    			/** Set that the target of the edge, j, has at least one incoming edge */
+	    			sccHasIncoming.set(tarjan.nodeSccMap[j]);
+	    			/** Set that the source of the edge, i, has at least one outgoing edge */
+	    			sccHasOutgoing.set(tarjan.nodeSccMap[i]);
+	    		}
+        	}   	
+        }
+    
+	    /** 
+	     * Determine count of "can reach all" nodes.
+	     * 
+	     * If *exactly one* SCC is a source, then the "can reach all" count is the number of nodes in that SCC.  
+	     * If there is more than one SCC that is a source - i.e. more than one zero in the bitset - then these nodes 
+	     * cannot get to each other, therefore there cannot be any node that can reach all others, 
+	     * so the "can reach all" count is zero.
+	     * 
+	     * The "has incoming/outgoing" bitsets created above contain the info on which SCCs have incoming/outgoing edges.
+	     */
+	    int canReachAll = 0;
+	    /** If exactly one bit is zero in the BitSet... */
+	    if(sccHasIncoming.cardinality() == tarjan.sccCount - 1) {
+	    	/** get the position of the one zero bit, which is the scc's index... */
+	 	    int pos = sccHasIncoming.nextClearBit(0);
+	 	    /** set the count to the number of nodes in that scc */
+	 	    canReachAll = scComponents.get(pos).size();
+	    }
+    
+	    /** 
+	     * Determine count of "reached by all" nodes.  The logic is the same as above for calculating canReachAll.
+	     * If *exactly one* SCC is a sink, then the count is the number of nodes in that SCC.
+	     */
+	    int reachedByAll = 0;
+	    if(sccHasOutgoing.cardinality() == tarjan.sccCount - 1) {
+	 	    int pos = sccHasOutgoing.nextClearBit(0);
+	 	    reachedByAll = scComponents.get(tarjan.nodeSccMap[pos]).size();
+	    }
+	    
+	    /** Return integer array containing the "can reach all" and "reached by all" counts. */
+		return new int[]{canReachAll, reachedByAll};
+	}
+}
